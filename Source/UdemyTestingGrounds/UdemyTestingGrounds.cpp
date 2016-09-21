c1d55e7bb33e4f65e0838a394b7cbd50567b1442
acxsasx <adamc_thompson@yahoo.com>
2016-09-21 19:32:46 -0400

    Initial commit

---
+++ Source/UdemyTestingGrounds/UdemyTestingGrounds.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#include "UdemyTestingGrounds.h"
+
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UdemyTestingGrounds, "UdemyTestingGrounds" );
+ 
